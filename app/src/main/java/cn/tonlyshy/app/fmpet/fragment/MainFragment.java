704f6a013fcf7020f5007bac9016e9b18d77e1ba
steiensGate <905495282@qq.com>
2017-05-17 04:21:05 +0800

    ActionBar back

--- app/src/main/java/cn/tonlyshy/app/fmpet/fragment/MainFragment.java
+++ app/src/main/java/cn/tonlyshy/app/fmpet/fragment/MainFragment.java
@@ -2,6 +2,8 @@ package cn.tonlyshy.app.fmpet.fragment;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,8 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.content_main, container, false);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
         return view;
     }
 }
